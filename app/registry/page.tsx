c96c586101fd622dae428e9517721998ddbda606
Ryan Cooper <jrwcooper@outlook.com>
2025-03-17 12:48:25 -0500

    Fix TypeScript error for mobileMenuRef.contains

--- app/registry/page.tsx
+++ app/registry/page.tsx
@@ -5,14 +5,14 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="w-full h-screen overflow-hidden flex flex-col items-center justify-center text-center p-4 space-y-8">
+    <div className="w-full overflow-auto flex flex-col items-center justify-center text-center p-4 pt-32 space-y-8">
       {/* Title */}
-      <h1 className="font-brush text-9xl font-bold text-[#BF9D3E] tracking-wide">
+      <h1 className="font-brush text-5xl sm:text-7xl md:text-9xl font-bold text-[#BF9D3E] tracking-wide">
         The Registry
       </h1>
 
       {/* Description */}
-      <p className="w-7/12 text-lg text-[#fefefe]">
+      <p className="w-full sm:w-10/12 md:w-7/12 text-base sm:text-lg text-[#fefefe]">
         We sincerely appreciate your presence on our special day, which is truly
         the greatest gift. However, if you’d like to help us begin this next
         chapter, we’ve put together a few registry options below. Whether you
@@ -21,9 +21,12 @@ export default function Page() {
       </p>
 
       {/* Logo Grid */}
-      <div className="grid grid-cols-3 gap-10 w-full max-w-4xl justify-center items-center">
-        {/* Williams Sonoma (linked) */}
-        <div className="flex items-center justify-center justify-self-center w-[20rem] h-[5rem] cursor-pointer">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 w-full max-w-4xl items-center justify-center">
+        {/* Williams Sonoma */}
+        <div
+          className="flex items-center justify-center w-full max-w-[150px] sm:max-w-md h-auto cursor-pointer justify-self-center
+"
+        >
           <Link
             href="https://www.williams-sonoma.com/registry/gc7gjzrm86/registry-list.html"
             target="_blank"
@@ -34,13 +37,16 @@ export default function Page() {
               alt="William Sonoma"
               width={3840}
               height={2160}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-contain"
             />
           </Link>
         </div>
 
         {/* Target */}
-        <div className="flex items-center justify-center justify-self-center w-[10rem] h-[10rem] cursor-pointer">
+        <div
+          className="flex items-center justify-center w-full max-w-[100px] sm:max-w-xs h-auto cursor-pointer justify-self-center
+"
+        >
           <Link
             href="https://www.target.com/gift-registry/gift/ryanandnicolsregistry"
             target="_blank"
@@ -51,13 +57,16 @@ export default function Page() {
               alt="Target"
               width={3840}
               height={2160}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-contain"
             />
           </Link>
         </div>
 
         {/* Amazon */}
-        <div className="flex items-center justify-center justify-self-center w-[15rem] h-[5rem] cursor-pointer">
+        <div
+          className="flex items-center justify-center w-full max-w-[120px] sm:max-w-sm h-auto cursor-pointer justify-self-center
+"
+        >
           <Link
             href="https://www.amazon.com/wedding/registry/3FX9KI0PYQFKY"
             target="_blank"
@@ -68,13 +77,13 @@ export default function Page() {
               alt="Amazon"
               width={3840}
               height={2160}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-contain"
             />
           </Link>
         </div>
 
         {/* Sur La Table */}
-        <div className="flex items-center justify-center justify-self-center w-[18rem] h-[5rem] col-start-2 cursor-pointer">
+        <div className="flex items-center justify-center w-full max-w-[150px] sm:max-w-md h-auto cursor-pointer md:col-span-1 md:col-start-2 justify-self-center">
           <Link
             href="https://www.surlatable.com/giftregistry-customershow?ID=9447acbcd4694f40a301494b3f"
             target="_blank"
@@ -85,7 +94,7 @@ export default function Page() {
               alt="Sur La Table"
               width={3840}
               height={2160}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-contain"
             />
           </Link>
         </div>
